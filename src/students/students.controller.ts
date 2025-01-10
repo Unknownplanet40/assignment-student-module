72471cb896922409ce450c60e5278b4f7a2b449d
Unknownplanet40 <ryanjamesc4@gmail.com>
2025-01-10 20:57:30 +0800

    read function has been added

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { Student } from './students.entity';
 
@@ -11,4 +11,13 @@ export class StudentsController {
         return this.studentService.createStudent(data);
     }
 
+    @Get()
+    async getStudent(): Promise<Student[]> {
+        return this.studentService.getStudents();
+    }
+
+    @Get(':id')
+    async getStudentById(@Param('id') id: number): Promise<Student> {
+        return this.studentService.getStudentById(id);
+    }
 }
